43b478b6460eb5fe9a573d30471fd6525bd3d7a1
Maedeh Farahmand <107555003+maedehfarahmand@users.noreply.github.com>
2025-11-29 18:47:52 -0500

    Delete prod directory

--- prod/backend_configs/backend-prod.hcl
+++
@@ -1,5 +0,0 @@
-bucket         = "group10-acs730-prod-tfstate"     
-key            = "two-tier-app/prod/terraform.tfstate"
-region         = "us-east-1"
-encrypt        = true
-dynamodb_table = "group10-acs730-terraform-locks"
--- prod/modules/security_group/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "alb_security_group_id" {
-  description = "The ID of the ALB Security Group"
-  value       = aws_security_group.alb_sg.id
-}
-
-output "instance_security_group_id" {
-  description = "The ID of the EC2 instance Security Group"
-  value       = aws_security_group.instance_sg.id
-}
